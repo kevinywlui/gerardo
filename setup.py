ac420591d1c0fdb8dda00ba36105d026d3b49e2d
kevin lui <kevinywlui@gmail.com>
2019-08-23 21:27:06 -0700

    Fix missing comma

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords="psql multiprocessing",
