75d724ff5db93b098512ab8a6135aff39eaf0b1e
kevin lui <kevinywlui@gmail.com>
2019-08-28 16:09:47 -0700

    Add more details to example

--- examples/example.py
+++ examples/example.py
@@ -1,6 +1,5 @@
 """Example usage of decorators.
 """
-import multiprocessing
 import os
 from gerardo import psql_insert, psql_mp_insert, psql_handler
 
@@ -26,11 +25,21 @@ COLUMNS = [('x', 'INT'), ('y', 'INT')]
 PH = psql_handler(DSN, PSQL_TABLE, COLUMNS)
 
 # This transform `f` into a function that applies `f` to a list of arguments in
-# parallel and inserts into a table
+# parallel and inserts into a table. Currently, we only handle the
+# single-variable case.
 @psql_mp_insert(PH)
 def f(x):
     return (x[0]+x[1], x[0]*x[1])
 
+
 # Actually compute with `f`.
-l = [(x,y) for x in range(5) for y in range(10)]
-f(l)
+list_args = [(x, y) for x in range(5) for y in range(10)]
+f(list_args)
+
+# Alternatively we can do this in not-parallel.
+@psql_insert(PH)
+def g(x, y):
+    return (x, x+y)
+
+
+g(3, 4)
