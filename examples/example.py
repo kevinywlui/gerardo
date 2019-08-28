472c2197cc163ef755664a10d89d4af7dbb64790
kevin lui <kevinywlui@gmail.com>
2019-08-28 16:28:59 -0700

    Style change

--- examples/example.py
+++ examples/example.py
@@ -7,19 +7,13 @@ from gerardo import psql_insert, psql_mp_insert, psql_handler
 pghost = os.environ["PGHOST"]
 
 # Insert into a table called `example_db`
-DSN = {
-    "host": pghost,
-    "port": "",
-    "user": "",
-    "password": "",
-    "dbname": "example_db",
-}
+DSN = {"host": pghost, "port": "", "user": "", "password": "", "dbname": "example_db"}
 
 # Table will be called `test_table`
 PSQL_TABLE = "test_table"
 
 # There are two columns called `x` and `y`. They both contain `INT`.
-COLUMNS = [('x', 'INT'), ('y', 'INT')]
+COLUMNS = [("x", "INT"), ("y", "INT")]
 
 # Set a psql handler
 PH = psql_handler(DSN, PSQL_TABLE, COLUMNS)
@@ -29,7 +23,7 @@ PH = psql_handler(DSN, PSQL_TABLE, COLUMNS)
 # single-variable case.
 @psql_mp_insert(PH)
 def f(x):
-    return (x[0]+x[1], x[0]*x[1])
+    return (x[0] + x[1], x[0] * x[1])
 
 
 # Actually compute with `f`.
@@ -39,7 +33,7 @@ f(list_args)
 # Alternatively we can do this in not-parallel.
 @psql_insert(PH)
 def g(x, y):
-    return (x, x+y)
+    return (x, x + y)
 
 
 g(3, 4)
